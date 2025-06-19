263d42794d4c2a9afe2a1a7bf1d8e0d6534f054d
messer <m-esser@mail.de>
2025-06-19 18:46:53 +0200

    rename git url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Unsplash-Crawler',
     version='1.0.0',
-    url='https://github.com/m-p-esser/immo-crawler.git',
+    url='https://github.com/m-p-esser/unsplash-crawler.git',
     author='Author Name',
     author_email='author@gmail.com',
     description='Description of my package',
